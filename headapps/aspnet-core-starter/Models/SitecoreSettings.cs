b9ff30d3305638a4d23b013b947fff9ea31e4b29
Rob Earlam <robert.earlam@gmail.com>
2024-08-05 11:37:47 +1000

    Initial stubbing out of starterkit middleware includes and related assets

---
+++ headapps/aspnet-core-starter/Models/SitecoreSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Sitecore.AspNetCore.Starter.Models
+{
+    public class SitecoreSettings
+    {
+        public static readonly string Key = "Sitecore";
+
+        public Uri? InstanceUri { get; set; }
+
+        public string LayoutServicePath { get; set; } = "/api/graphql/v1";
+
+        public string? DefaultSiteName { get; set; }
+
+        public string? ExperienceEdgeToken { get; set; }
+
+        public Uri? LayoutServiceUri
+        {
+            get
+            {
+                if (InstanceUri == null) return null;
+
+                return new Uri(InstanceUri, LayoutServicePath);
+            }
+        }
+    }
+}
--- headapps/aspnet-core-starter/Program.cs
+++ headapps/aspnet-core-starter/Program.cs
@@ -1,6 +1,49 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var sitecoreSettings = builder.Configuration.GetSection(SitecoreSettings.Key).Get<SitecoreSettings>();
+ArgumentNullException.ThrowIfNull(sitecoreSettings);
+
+builder.Services.AddRouting()
+                .AddLocalization()
+                .AddMvc();
+
+builder.Services.AddSitecoreLayoutService()
+                .AddSystemTextJson()
+                .AddGraphQlHandler("default", sitecoreSettings.DefaultSiteName!, sitecoreSettings.ExperienceEdgeToken!, sitecoreSettings.LayoutServiceUri!)
+                .AsDefaultHandler();
+
+builder.Services.AddSitecoreRenderingEngine(options =>
+                    {
+                        options.AddDefaultPartialView("_ComponentNotFound");
+                    })
+                .ForwardHeaders();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseRequestLocalization(options =>
+{
+    options.UseSitecoreRequestLocalization();
+});
+
+app.MapControllerRoute(
+    "error",
+    "error",
+    new { controller = "Default", action = "Error" }
+);
+app.MapSitecoreLocalizedRoute("sitecore", "Index", "Default");
+app.MapFallbackToController("Index", "Default");
 
 app.Run();
