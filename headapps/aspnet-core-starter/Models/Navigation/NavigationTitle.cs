b5ba0d10693c84da93949b32f9e316bf43d45772
Rob Earlam <robert.earlam@gmail.com>
2024-09-11 15:36:01 +1000

    Bumped SDK version, implemented Navigation Component

---
+++ headapps/aspnet-core-starter/Models/Navigation/NavigationTitle.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.AspNetCore.Starter.Models.Navigation
+{
+    public class NavigationTitle
+    {
+        public string? Value { get; set; }
+
+        public bool? Editable { get; set; }
+    }
+}
