90681cfc3205c69ca48c4fa86e1b08484ff21fed
Rob Earlam <robert.earlam@gmail.com>
2024-09-16 14:43:39 +1000

    Removed unneeded Model

--- headapps/aspnet-core-starter/Models/Navigation/NavigationTitle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sitecore.AspNetCore.Starter.Models.Navigation
-{
-    public class NavigationTitle
-    {
-        public string? Value { get; set; }
-
-        public bool? Editable { get; set; }
-    }
-}
